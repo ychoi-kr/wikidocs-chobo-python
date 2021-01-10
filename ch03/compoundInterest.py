6b4b0e7c1fec92b6380b8cbf255b7a2175f1e9cc
Yong Choi <sk8er.choi@gmail.com>
2021-01-11 06:10:49 +0900

    Update compoundInterest.py
    파라미터 순서 변경, 반환 타입 변환 제거

--- ch03/compoundInterest.py
+++ ch03/compoundInterest.py
@@ -1,9 +1,9 @@
-def compound_interest_amount(p, r, n, t):
-    return int(p * (1 + r / n) ** (n * t))
+def compound_interest_amount(p, r, t, n):
+    return p * (1 + r / n) ** (n * t)
 
 if __name__ == '__main__':
     # ex 1
-    print(compound_interest_amount(1500000, 0.043, 4, 6))
+    print(compound_interest_amount(1500000, 0.043, 6, 4))
 
     # ex 2
-    print(compound_interest_amount(1500000, 0.043, 1/2, 6))
+    print(compound_interest_amount(1500000, 0.043, 6, 1/2))
