19216b501352e99049944efdef80a89864fef81f
Yong Choi <sk8er.choi@gmail.com>
2021-01-10 22:27:07 +0900

    Create compoundInterest.py

---
+++ ch03/compoundInterest.py
@@ -0,0 +1,9 @@
+def compound_interest_amount(P, r, n, t):
+    return int(P * (1 + r / n) ** (n * t))
+
+if __name__ == '__main__':
+    # ex 1
+    print(compound_interest_amount(1500000, 0.043, 4, 6))
+
+    # ex 2
+    print(compound_interest_amount(1500000, 0.043, 1/2, 6))
