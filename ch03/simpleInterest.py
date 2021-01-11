36c3fe1f043359df412516e7eaee5ae743083090
Yong Choi <sk8er.choi@gmail.com>
2021-01-11 22:33:15 +0900

    Update simpleInterest.py

--- ch03/simpleInterest.py
+++ ch03/simpleInterest.py
@@ -1,18 +1,14 @@
 def simple_interest(p, r, t):
-    return int(p * r * t)
+    return p * r * t
     
 def simple_interest_amount(p, r, t):
-    return int(p * (1 + r * t))
+    return p * (1 + r * t)
 
 if __name__ == '__main__':
-    # Quiz 1
-    # ex 1
-    print(simple_interest(10000000, 0.03875, 5))
-    # ex 2
-    print(simple_interest(1100000, 0.05, 5/12))
+    print('Quiz 1')
+    print('ex 1:', simple_interest(10000000, 0.03875, 5))
+    print('ex 2:', simple_interest(1100000, 0.05, 5/12))
 
-    # Quiz 2
-    # ex 1
-    print(simple_interest_amount(10000000, 0.03875, 5))
-    # ex 2
-    print(simple_interest_amount(1100000, 0.05, 5/12))
+    print('\nQuiz 2')
+    print('ex 1:', simple_interest_amount(10000000, 0.03875, 5))
+    print('ex 2:', simple_interest_amount(1100000, 0.05, 5/12))
