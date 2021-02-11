1b4b5443e38ec6459b43ef8161b6f1bdc7536fed
Yong Choi <sk8er.choi@gmail.com>
2021-02-11 19:09:18 +0900

    Update prime.py

--- ch02/prime.py
+++ ch02/prime.py
@@ -1,9 +1,10 @@
 n = int(input())
 L = list(range(2, n))
+L2 = L[:] # https://stackoverflow.com/a/10665602
 
 for p in L:
     for q in L:
-        if q != p and q % p == 0:
-            L.remove(q)
+        if (q in L2) and (q != p and q % p == 0):
+            L2.remove(q)
 
-print(L)
+print(L2)
