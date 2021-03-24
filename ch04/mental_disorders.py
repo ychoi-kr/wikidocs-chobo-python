3c1cc7a8956a2bf1618ff0cb563b462ff879d788
Yong Choi <sk8er.choi@gmail.com>
2021-03-24 21:25:01 +0900

    Create mental_disorders.py

---
+++ ch04/mental_disorders.py
@@ -0,0 +1,34 @@
+txt = '''신경발달장애 Neurodevelopmental Disorders
+조현병 스펙트럼 및 기타 정신병적 장애 Schizophrenia Spectrum and Other Psychotic Disorders
+양극성 및 관련 장애 Bipolar and Related Disorders
+우울장애 Depressive Disorders
+불안장애 Anxiety Disorder
+강박 및 관련 장애 Obsessive－Compulsive and Related Disorders
+외상 및 스트레스 관련 장애 Trauma－and Stressor－Related Disorders
+해리장애 Dissociative Disorders
+신체증상 및 관련 장애 Somatic Symptom and Related Disorders
+급식 및 섭식장애 Feeding and Eating Disorders
+배설장애 Elimination Disorders
+수면－각성 장애 Sleep－Wake Disorders
+성기능부전 Sexual Dysfunctions
+성별 불쾌감 Gender Dysphoria
+파괴적, 충동조절 및 품행 장애 Disruptive, Impulse－Control, and Conduct Disorders
+물질관련 및 중독 장애 Substance－Related and Addictive Disorders
+신경인지장애 Neurocognitive Disorders
+성격장애 Personality Disorders
+변태성욕장애 Paraphilic Disorders
+기타 정신질환 Other Mental Disorders'''
+
+disorders = dict()
+
+is_eng = lambda x: 65 <= ord(x) <= 90 or 97 <= ord(x) <= 122
+
+for l in txt.split('\n'):
+    i = 0
+    while not is_eng(l[i]):
+        i += 1
+    else:
+        ko, en = l[:i - 1], l[i:]
+        disorders[ko] = en
+
+print(disorders)
