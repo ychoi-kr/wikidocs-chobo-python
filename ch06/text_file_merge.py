524465d842074036ee546de825056e5ed4f8e28b
Yong Choi <sk8er.choi@gmail.com>
2021-03-29 13:04:28 +0900

    create text file merge exercise

---
+++ ch06/text_file_merge.py
@@ -0,0 +1,11 @@
+import glob
+
+outfile = open("out.txt", "wt")
+
+for x in glob.glob("korean_national_anthem_?.txt"):
+    infile = open(x, "rt")
+    #infile = open(x, "rt", encoding="utf-8-sig") # https://stackoverflow.com/a/44573867
+    
+    outfile.write(x + '\n')
+    outfile.write('-' * len(x) + '\n')
+    outfile.write(infile.read() + "\n\n")
