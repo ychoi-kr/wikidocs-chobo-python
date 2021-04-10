7d5f24290e6c4bec0fa5b40eb130a334ee37f3d8
Yong Choi <sk8er.choi@gmail.com>
2021-04-10 21:26:00 +0900

    Create korean_1_to_10.py

---
+++ ch03/korean_1_to_10.py
@@ -0,0 +1,21 @@
+def korean_number(num):
+    if num == 1:
+        return '일'
+    elif num == 2:
+        return '이'
+    elif num == 3:
+        return '삼'
+    elif num == 4:
+        return '사'
+    elif num == 5:
+        return '오'
+    elif num == 6:
+        return '육'
+    elif num == 7:
+        return '칠'
+    elif num == 8:
+        return '팔'
+    elif num == 9:
+        return '구'
+    elif num == 10:
+        return '십'
