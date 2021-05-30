8182871ee81fded4ede24e4c0588ac74cfe04a7a
Yong Choi <sk8er.choi@gmail.com>
2021-05-30 16:58:50 +0900

    Create exercise "English quiz"

---
+++ ch06/english_quiz.py
@@ -0,0 +1,30 @@
+import random
+
+d = dict()
+
+with open('ko_en.txt') as f:
+    for line in f.readlines()[1:]:
+        k, v = tuple(line.split('\t'))
+        d[k] = v
+
+quiz = list(d.keys())
+random.shuffle(quiz)
+
+while True:
+    if len(quiz) == 0:
+        break
+    
+    q = quiz.pop()
+    print("Write the following sentences in English.")
+    print(q)
+    a = input("\nyour answer: ")
+
+    if a == d[q].rstrip():
+        print('\nresult: Correct!')
+    else:
+        print("\nresult: Not correct!")
+        print("right answer:" + d[q].rstrip() + '\n')
+
+    input()
+
+    print('-' * 80)
