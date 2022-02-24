7e07693ad483e2c6787a94daadba826db60449c3
sk8erchoi <sk8er.choi@gmail.com>
2022-02-24 20:55:25 +0900

    add rule90.py

---
+++ ch04/rule90.py
@@ -0,0 +1,14 @@
+colwidth = 79 
+rule90 = {'000':'0', '001':'1', '010':'0', '011':'1', '100':'1', '101':'0', '110':'1', '111':'0'}
+
+half = colwidth // 2
+line = '0' * half + '1' + '0' * half
+print(line)
+
+while line[1] == '0':
+    prev = line
+    line = '0' * colwidth
+    for i in range(1, colwidth - 1):
+        line = line[:i] + rule90[prev[i-1:i+2]] + line[i+1:]
+    print(line)
+    
