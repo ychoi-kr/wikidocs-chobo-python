5fcaac887cfea2ca4da29adebdc0014cefb44a98
Yong Choi <sk8er.choi@gmail.com>
2022-11-04 22:26:31 +0900

    Update gen_password.py

--- ch05/gen_password.py
+++ ch05/gen_password.py
@@ -6,16 +6,16 @@ len_alphabet = 10
 len_digit = 4
 len_special = 1
 
-candidates = list()
+letters = list()
 
 for l in range(len_alphabet):
-    candidates.append(random.choice(list(set(string.ascii_letters) - set('lIO'))))
+    letters.append(random.choice(list(set(string.ascii_letters) - set('lIO'))))
 
 for d in range(len_digit):
-    candidates.append(random.choice(list(set(string.digits) - set('01'))))
+    letters.append(random.choice(list(set(string.digits) - set('01'))))
 
 for s in range(len_special):
-    candidates.append(random.choice('!@#$%^&*?'))
+    letters.append(random.choice('!@#$%^&*?'))
 
-random.shuffle(candidates)
-print(''.join(candidates))
+random.shuffle(letters)
+print(''.join(letters))
