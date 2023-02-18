49aba27a06d4ad2bdc6cb4d91beb8d82f9be6273
Yong Choi <ychoi_kr@naver.com>
2023-02-18 17:01:50 +0900

    create generations.py

---
+++ ch02/generations.py
@@ -0,0 +1,19 @@
+y = int(input('What year were you born? '))
+
+gen = None
+
+if y <= 1924:
+    gen = 'the Greatest Generation'
+elif y <= 1945:
+    gen = 'the Silent Generation'
+elif y <= 1964:
+    gen = 'a baby boomers'
+elif y <= 1980:
+    gen = 'a Gen X'
+elif y <= 1996:
+    gen = 'a millennial'
+else:
+    gen = 'a Gen Z'
+
+print(f"You're {gen}.")
+
