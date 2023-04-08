c237e377c0157a24310a07eaee59f9458fc3b10e
Yong Choi <ychoi_kr@naver.com>
2023-04-08 10:46:21 +0900

    create animal.py

---
+++ ch07/animal.py
@@ -0,0 +1,14 @@
+class 동물:
+    def 울어(self):
+        print('...') 
+
+class 고양잇과(동물):
+    pass
+
+class 호랑이(고양잇과):
+    def 울어(self):
+        print('어흥')
+
+class 고양이(고양잇과):
+    def 울어(self):
+        print('야옹')
