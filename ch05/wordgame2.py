5e1fde6bcab7fb1c286c5851e9717d98654a2226
Yong Choi <ychoi_kr@naver.com>
2023-06-04 20:10:38 +0900

    update word games

--- ch05/wordgame2.py
+++ ch05/wordgame2.py
@@ -1,5 +1,7 @@
 my_voca = """
 기차
+차량
+양심
 차박
 박쥐
 쥐구멍
@@ -21,32 +23,37 @@ my_voca = """
 자율
 """.split()
 
-with open("korean_words.txt") as f:
-    voca = set(f.read().split()) 
-
 init_word = '기차'
 print('끝말잇기를 하자. 내가 먼저 말할게.\n' + init_word)
 spoken_words = [init_word]
 
+
+def trans_first(word):
+    s1 = "라락란량로료리"
+    s2 = "나낙난양노뇨이"
+    tt = str.maketrans(s1, s2)
+    return word[0].translate(tt) + word[1:]
+
+
 while True:
     user_input = input('> ')
-
+    
     if not user_input:
         continue
+    elif user_input == "졌다":
+        print('야호!')
+        break
     elif user_input[0] != spoken_words[-1][-1]:
         print('글자가 안 이어져. 내가 이겼다!')
         break
     elif user_input in spoken_words:
         print('아까 했던 말이야. 내가 이겼어!')
         break
-    elif user_input not in voca:
-        print('사전에 없는 말이야. 내가 이겼지롱~')
-        break
     else:
         spoken_words.append(user_input)
         my_word = None
-        for word in list(set(voca) - set(spoken_words)):
-            if word.startswith(user_input[-1]):
+        for word in list(set(my_voca) - set(spoken_words)):
+            if word.startswith(trans_first(user_input[-1])):
                 my_word = word
                 break
        
@@ -55,8 +62,6 @@ while True:
             break
         else:
             print(my_word)
-            if my_word not in voca:
-                print('사전에 없는 말이네. 내가 졌어.ㅠ')
             spoken_words.append(my_word)
             continue
     
--- ch06/wordgame3.py
+++ ch06/wordgame3.py
@@ -21,13 +21,24 @@ my_voca = """
 자율
 """.split()
 
+with open("korean_words.txt") as f:
+    voca = set(f.read().split()) 
+
 init_word = '기차'
 print('끝말잇기를 하자. 내가 먼저 말할게.\n' + init_word)
 spoken_words = [init_word]
 
+
+def trans_first(word):
+    s1 = "라락란량로료리"
+    s2 = "나낙난양노뇨이"
+    tt = str.maketrans(s1, s2)
+    return word[0].translate(tt) + word[1:]
+
+
 while True:
     user_input = input('> ')
-    
+
     if not user_input:
         continue
     elif user_input == "졌다":
@@ -39,11 +50,14 @@ while True:
     elif user_input in spoken_words:
         print('아까 했던 말이야. 내가 이겼어!')
         break
+    elif user_input not in voca:
+        print('사전에 없는 말이야. 내가 이겼지롱~')
+        break
     else:
         spoken_words.append(user_input)
         my_word = None
-        for word in list(set(my_voca) - set(spoken_words)):
-            if word.startswith(user_input[-1]):
+        for word in list(set(voca) - set(spoken_words)):
+            if word.startswith(trans_first(user_input[-1])):
                 my_word = word
                 break
        
@@ -52,6 +66,8 @@ while True:
             break
         else:
             print(my_word)
+            if my_word not in voca:
+                print('사전에 없는 말이네. 내가 졌어.ㅠ')
             spoken_words.append(my_word)
             continue
     
