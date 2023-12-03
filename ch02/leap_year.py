23c172b3ee180b45da83b44918539d762b873b76
Yong Choi <ychoi_kr@naver.com>
2023-12-03 11:32:47 +0900

    remove while statement to match with flow diagram

--- ch02/leap_year.py
+++ ch02/leap_year.py
@@ -1,20 +1,19 @@
-while True:
-    is_leap_year = None
+is_leap_year = None
 
-    year = int(input())
+year = int(input())
 
-    if year % 4 == 0:
-        if year % 100 == 0:
-            if year % 400 == 0:
-                is_leap_year = True
-            else:
-                is_leap_year = False
-        else:
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
             is_leap_year = True
+        else:
+            is_leap_year = False
     else:
-        is_leap_year = False
+        is_leap_year = True
+else:
+    is_leap_year = False
 
-    if is_leap_year:
-        print(f'{year} is a leap year')
-    else:
-        print(f'{year} is not a leap year')
+if is_leap_year:
+    print(f'{year} is a leap year')
+else:
+    print(f'{year} is not a leap year')
