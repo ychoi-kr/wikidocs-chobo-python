fe3e94cb62f7203272d22756943a814aec182416
Yong Choi <sk8er.choi@gmail.com>
2024-01-25 16:15:15 +0900

    Create rational_operation.py

---
+++ ch04/rational_operation.py
@@ -0,0 +1,9 @@
+sa, sb = input().split()
+na, nb = int(sa), int(sb)
+
+addition = na + nb
+subtraction = na - nb
+multiplication = na * nb
+division = na / nb
+
+print(addition, subtraction, multiplication, division)
